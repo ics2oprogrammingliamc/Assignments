c4496419c59a8693dcfa4eb1df3a609df59cd79e
Liam Csiffary <47784162+s299776@users.noreply.github.com>
2019-04-12 08:48:41 -0400

    CompanyAnimation

--- CompanyAnimation(LiamC)/main.lua
+++ CompanyAnimation(LiamC)/main.lua
@@ -7,4 +7,50 @@
 -- Your code here
 
 display.setStatusBar(display.HiddenStatusBar)
-local 
+display.setDefault("background", 0, 1, 1)
+----------------------------------------------------------------
+--                              Variables + Objectsd
+-------------------------------------------------------------------
+-- create company logo
+local companyLogo = display.newImageRect("Images/CompanyLogoLiamC.@2x-Recovered.png", 100, 100)
+companyLogo.x = 0
+companyLogo.y = display.contentHeight/2
+
+local companyLogo2 = display.newImageRect("Images/CompanyLogoLiamC.@2x-Recovered.png", 100, 100)
+companyLogo2.isVisible = false
+companyLogo2.x = display.contentWidth/2
+companyLogo2.y = display.contentHeight/2
+companyLogo2:scale(2.5, 2.5)
+
+-- set the scroll speed
+local scrollSpeed = 3
+
+-- create local for rotation
+local rotate = 0
+
+--------------------------------------------------------------------
+--                           FUNCTIONS
+----------------------------------------------------------------------
+
+local function AnimateCompanyLogo(event)
+	-- add the scroll speed to the x co-ordinate of the logo
+	companyLogo.x = companyLogo.x + scrollSpeed
+
+	rotate = rotate + scrollSpeed
+
+	companyLogo.rotation = rotate
+end
+
+local function PopUpLogo()
+	companyLogo2.isVisible = true
+end
+--------------------------------------------------------------------------------
+--							Timers
+----------------------------------------------------------------------------------------------------------------------
+
+timer.performWithDelay(6000, PopUpLogo)
+
+-----------------------------------------------------------------------
+--                         Event Listeners
+---------------------------------------------------------------------------------
+Runtime:addEventListener("enterFrame", AnimateCompanyLogo)
