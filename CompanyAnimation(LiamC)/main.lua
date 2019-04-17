c3045ba2c6809536bc17b0daf02cf9149d0125d0
Liam Csiffary <47784162+s299776@users.noreply.github.com>
2019-04-17 09:33:49 -0400

    CompanyAnimation

--- CompanyAnimation(LiamC)/main.lua
+++ CompanyAnimation(LiamC)/main.lua
@@ -10,15 +10,21 @@
 display.setStatusBar(display.HiddenStatusBar)
 display.setDefault("background", 0, 1, 1)
 ----------------------------------------------------------------
---                              LOCALS Variables
+--                              Variables + Objectsd
 -------------------------------------------------------------------
 -- create company logo
-local companyLogo 
+local companyLogo = display.newImageRect("Images/CompanyLogoLiamC.@2x-Recovered.png", 100, 100)
+companyLogo.x = 0
+companyLogo.y = display.contentHeight/2
 
-local companyLogo2
+local companyLogo2 = display.newImageRect("Images/CompanyLogoLiamC.@2x-Recovered.png", 100, 100)
+companyLogo2.isVisible = false
+companyLogo2.x = display.contentWidth/2
+companyLogo2.y = display.contentHeight/2
+companyLogo2:scale(3.5, 3.5)
 
 -- set the scroll speed
-local scrollSpeed = 9
+local scrollSpeed = 3
 
 -- create local for rotation
 local rotate = 0
@@ -27,34 +33,29 @@ local visible = 0
 
 local scale = 1
 --------------------------------------------------------------------
---                           OBJECT CREATION
-----------------------------------------------------------------------
-companyLogo = display.newImageRect("Images/CompanyLogoLiamC.@2x-Recovered.png", 100, 100)
-companyLogo.x = 0
-companyLogo.y = display.contentHeight/2
-
-companyLogo2 = display.newImageRect("Images/CompanyLogoLiamC.@2x-Recovered.png", 100, 100)
-companyLogo2.isVisible = false
-companyLogo2.x = display.contentWidth/2
-companyLogo2.y = display.contentHeight/2
-companyLogo2:scale(3.5, 3.5)
---------------------------------------------------------------------
---                           LOCAL FUNCTIONS
+--                           FUNCTIONS
 ----------------------------------------------------------------------
 
 local function AnimateCompanyLogo(event)
 	-- add the scroll speed to the x co-ordinate of the logo
 	companyLogo.x = companyLogo.x + scrollSpeed
 
+	-- MAKES THE IMAGE rotate
 	rotate = rotate + scrollSpeed
 
+	-- makes it more and more visible as time goes on
 	companyLogo.alpha = visible + .5
 
+	-- makes the logo grow
 	companyLogo:scale( scale, scale )
 
-	scale = scale + .00009
+	-- makes thhe logo grow
+	scale = scale + .00001
 
-	visible = visible + .005
+	-- makes the logo be increasingly more visible
+	visible = visible + .002
+
+	-- makes the logo rotate
 	companyLogo.rotation = rotate
 end
 
@@ -66,7 +67,7 @@ end
 --							Timers
 ----------------------------------------------------------------------------------------------------------------------
 
-timer.performWithDelay(2500, PopUpLogo)
+timer.performWithDelay(6000, PopUpLogo)
 
 -----------------------------------------------------------------------
 --                         Event Listeners
